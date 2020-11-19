fd7d59e7fbf65c9e115a921f37d71e987d5b907d
fufukha <wafakhalid29@gmail.com>
2020-11-19 06:51:18 -0500

    styles for keyboard and keys

--- src/components/Key/index.js
+++ src/components/Key/index.js
@@ -8,8 +8,36 @@ import styles from './key.css'
 const Key = ({ value }) => {
   const pressedKey = useSelector(charSelector)
 
+  const isEn = () => value.match(/^[a-zA-Z]/);
+  const isMedium = value === 'delete' || value === 'tab'
+  const isLarge = value === 'caps lock' || value === 'return'
+  const isRightAlign = value === 'delete' || value === 'return' || value.endsWith('right')
+  const isLeftAlign = value === 'tab' || value === 'caps lock' || value.endsWith('left')
+  const isBlank = value.startsWith('opt')
+  const getValue = () => {
+    if(value === 'space') return ''
+    if(value.startsWith('shift')) return 'shift'
+    return value
+  }
+  
   return (
-    <div className={classnames({ [styles.pressed]: pressedKey === value })}>{value}</div>
+    <div
+      dataLang={isEn()? 'en' : 'ar'}
+      className={classnames( styles.key,
+      { [styles.key_med]: isMedium,
+        [styles.key_large]: isLarge,
+        [styles.key_xl]: value.startsWith('shift'),
+        [styles.key_space]: value === 'space',
+        [styles.key_optLeft]: value === 'opt-left',
+        [styles.key_optRight]: value === 'opt-right',
+        [styles.key_leftAlign]: isLeftAlign,
+        [styles.key_rightAlign]: isRightAlign,
+        [styles.key_blank]: isBlank,
+        [styles.pressed]: pressedKey === value,
+       }
+    )}>
+      {getValue()}
+    </div>
   );
 }
 
--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Key from '../Key'
+import styles from './keyboard.css'
 
 const Keyboard = () => {
   const qwerty = [
-    'ذ', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩', '٠', '-', '=',
-    'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج', 'د', '\\',
-    'cap lock', 'ش', 'س', 'ي', 'ب', 'ل', 'ا', 'ت', 'ن', 'م', 'ك', 'ط', 'return',
-    'shift', 'ئ', 'ء', 'ؤ', 'ر', 'لا', 'ى', 'ة', 'و', 'ز', 'ظ', 'shift',
-    'space'
+    'ذ', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩', '٠', '-', '=', 'delete',
+    'tab', 'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج', 'د', '\\',
+    'caps lock', 'ش', 'س', 'ي', 'ب', 'ل', 'ا', 'ت', 'ن', 'م', 'ك', 'ط', 'return',
+    'shift-left', 'ئ', 'ء', 'ؤ', 'ر', 'لا', 'ى', 'ة', 'و', 'ز', 'ظ', 'shift-right',
+    'opt-left', 'space', 'opt-right'
   ];
 
   const keys = qwerty.map((key, i) => (
@@ -17,7 +18,7 @@ const Keyboard = () => {
 
 
   return (
-    <div>
+    <div className={styles.keyboard}>
       {keys}
     </div>
   );

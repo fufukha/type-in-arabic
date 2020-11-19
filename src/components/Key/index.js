c3972b050f66e8aebcbbbb81ff5cdb1d938d9d3e
fufukha <wafakhalid29@gmail.com>
2020-11-19 16:41:33 -0500

    pass qwerty data to Key and clean up classnames

--- src/components/Key/index.js
+++ src/components/Key/index.js
@@ -5,44 +5,26 @@ import { charSelector } from '../../state/selectors'
 import classnames from 'classnames'
 import styles from './key.css'
 
-const Key = ({ value }) => {
+const Key = ({ data }) => {
   const pressedKey = useSelector(charSelector)
+  const { size, align, value, keyName, lang, zone } = data
 
-  const isEn = () => value.match(/^[a-zA-Z]/);
-  const isMedium = value === 'delete' || value === 'tab'
-  const isLarge = value === 'caps lock' || value === 'return'
-  const isRightAlign = value === 'delete' || value === 'return' || value.endsWith('right')
-  const isLeftAlign = value === 'tab' || value === 'caps lock' || value.endsWith('left')
-  const isBlank = value.startsWith('opt')
-  const getValue = () => {
-    if(value === 'space') return ''
-    if(value.startsWith('shift')) return 'shift'
-    return value
-  }
-  
   return (
     <div
-      dataLang={isEn()? 'en' : 'ar'}
+      datalang={lang}
       className={classnames( styles.key,
-      { [styles.key_med]: isMedium,
-        [styles.key_large]: isLarge,
-        [styles.key_xl]: value.startsWith('shift'),
-        [styles.key_space]: value === 'space',
-        [styles.key_optLeft]: value === 'opt-left',
-        [styles.key_optRight]: value === 'opt-right',
-        [styles.key_leftAlign]: isLeftAlign,
-        [styles.key_rightAlign]: isRightAlign,
-        [styles.key_blank]: isBlank,
-        [styles.pressed]: pressedKey === value,
-       }
+        styles[`key_${size}`],
+        styles[`key_${align}Align`],
+        styles[`key_zone${zone}`],
+        { [styles.pressed]: keyName === pressedKey }
     )}>
-      {getValue()}
+      {value}
     </div>
   );
 }
 
 Key.propTypes = {
-  value: PropTypes.string.isRequired
+  data: PropTypes.object.isRequired
 };
 
 export default Key;
--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import Key from '../Key'
+import { getQwertyData } from './qwerty'
 import styles from './keyboard.css'
 
 const Keyboard = () => {
-  const qwerty = [
-    'ذ', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩', '٠', '-', '=', 'delete',
-    'tab', 'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج', 'د', '\\',
-    'caps lock', 'ش', 'س', 'ي', 'ب', 'ل', 'ا', 'ت', 'ن', 'م', 'ك', 'ط', 'return',
-    'shift-left', 'ئ', 'ء', 'ؤ', 'ر', 'لا', 'ى', 'ة', 'و', 'ز', 'ظ', 'shift-right',
-    'opt-left', 'space', 'opt-right'
-  ];
+  const qwerty = getQwertyData()
+  console.log(qwerty)
 
-  const keys = qwerty.map((key, i) => (
-      <Key key={i} value={key} />
+  const keys = qwerty.map((data, i) => (
+      <Key key={i} data={data} />
     )
   )
 

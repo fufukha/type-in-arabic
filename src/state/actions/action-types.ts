10881f649fcb7e1e91fc69020fb101a25abf0bbd
fufukha <wafakhalid29@gmail.com>
2021-01-11 12:33:14 -0500

    typed actions

---
+++ src/state/actions/action-types.ts
@@ -0,0 +1,21 @@
+import { Action } from 'redux';
+
+export const START_SESSION = 'STATE_SESSION'
+export const INPUT_CHAR = 'INPUT_CHAR'
+
+export interface StartSessionAction extends Action {
+  type: typeof START_SESSION
+  payload: {
+    prompt: string
+  }
+}
+
+export interface InputCharAction extends Action {
+  type: typeof INPUT_CHAR
+  payload: {
+      char: string,
+      timestamp: Date & number
+    }
+}
+
+export type AppActions = StartSessionAction | InputCharAction
---
+++ src/state/actions/index.ts
@@ -0,0 +1,22 @@
+import { ActionCreator } from 'redux';
+import { START_SESSION, INPUT_CHAR, StartSessionAction, InputCharAction } from './action-types'
+
+export const startSession: ActionCreator<StartSessionAction> = () => {
+  const prompt = 'نتنت'
+  return {
+    type: START_SESSION,
+    payload: {
+      prompt
+    }
+  }
+}
+
+export const inputChar: ActionCreator<InputCharAction> = (char: string, timestamp: number & Date) => {
+  return {
+    type: INPUT_CHAR,
+    payload: {
+      char,
+      timestamp
+    }
+  }
+}

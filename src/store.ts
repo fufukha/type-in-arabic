3d8944edaa8bbffb6fc9f327e6d0203ed2c51356
fufukha <wafakhalid29@gmail.com>
2021-01-12 10:11:09 -0500

    add Store type to store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,11 @@
-import { applyMiddleware, createStore,} from 'redux'
+import { applyMiddleware, createStore, Store } from 'redux'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import appReducer from './state/reducers/index'
 import { LevelState }  from './state/level-state'
+import { AppActions } from './state/actions/action-types'
 
-const store = createStore(
+const store: Store<LevelState, AppActions>  = createStore(
   appReducer,
   composeWithDevTools(
     applyMiddleware(logger),

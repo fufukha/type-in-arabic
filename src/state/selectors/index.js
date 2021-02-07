c7ac0745c137dfbd6939cf12f54368227d92d280
fufukha <wafakhalid29@gmail.com>
2021-02-07 07:34:17 -0500

    added promptSelector

---
+++ src/state/selectors/index.js
@@ -0,0 +1,22 @@
+export const isSessionCompletedSelector = state => {
+  if(state.prompt ) return state.index === state.prompt.length
+}
+
+export const errorsSelector = state => state.errors
+
+export const startTimeSelector = state => state.time
+
+export const cpmSelector = state => {
+  const { startTime, lastCharAt, index } = state
+
+  if(lastCharAt === null) return 0
+
+  const minutes = (lastCharAt - startTime) / 60000
+  return index / minutes
+}
+
+export const charSelector = state => state.char
+
+export const indexSelector = state => state.index
+
+export const promptSelector = state => state.prompt

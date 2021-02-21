21c14f2f610e0c17ef6aee3933d717ba9c59b69f
fufukha <wafakhalid29@gmail.com>
2021-02-21 12:34:12 -0500

    set and read local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import Keyfield from './components/Keyfield/index'
 import Keyboard from './components/Keyboard/index'
 import Stats from './components/Stats/index'
@@ -6,28 +7,45 @@ import {
   errorsSelector,
   cpmSelector,
   isSessionCompletedSelector,
+  levelSelector,
   hasStartedSelector,
 } from './state/selectors'
-import { 
-  startSession as startSessionAction,
-  nextLevel,
- } from './state/actions'
+import { startSession, nextLevel } from './state/actions'
 
 const App: React.FC = () => {
   const dispatch = useDispatch()
   const errors = useSelector(errorsSelector)
   const cpm = useSelector(cpmSelector)
   const isTaskCompleted = useSelector(isSessionCompletedSelector)
+  const level = useSelector(levelSelector)
   const hasStarted = useSelector(hasStartedSelector)
 
-  const startLevelHandler = (e: MouseEvent | React.MouseEvent) => {
-    dispatch(startSessionAction())
+  enum Local {
+    LAST_LEVEL_REACHED = 'lastLevelReached',
+  }
+
+  const initialStartHandler = (e: MouseEvent | React.MouseEvent) => {
+    e.preventDefault()
+    const lastLevel = localStorage.getItem(Local.LAST_LEVEL_REACHED)
+    const startLevel = lastLevel === null ? 0 : parseInt(lastLevel)
+    dispatch(startSession(startLevel))
+  }
+
+  const redoLevelHandler = (e: MouseEvent | React.MouseEvent) => {
+    e.preventDefault()
+    dispatch(startSession(level))
   }
 
   const nextLevelHandler = (e: MouseEvent | React.MouseEvent) => {
     e.preventDefault()
     dispatch(nextLevel())
   }
+
+  useEffect(() => {
+    if (level !== null) {
+      localStorage.setItem(Local.LAST_LEVEL_REACHED, level.toString())
+    }
+  }, [level, Local])
 
   return (
     <>
@@ -47,7 +65,7 @@ const App: React.FC = () => {
         <>
           <div>Task completed</div>
           <Stats errors={errors} cpm={cpm} />
-          <button onClick={startLevelHandler}>REDO</button>
+          <button onClick={redoLevelHandler}>REDO</button>
           <button onClick={nextLevelHandler}>NEXT</button>
         </>
       )}

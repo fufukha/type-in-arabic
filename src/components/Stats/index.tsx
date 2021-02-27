a225ec1ac276ec500b14d081e4961930d4b11f3a
fufukha <wafakhalid29@gmail.com>
2021-02-27 06:37:06 -0500

    moved redo and next button moved Stat component from App

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -1,13 +1,23 @@
 type StatsProps = {
-	errors: number;
-	cpm: number;
-};
-const Stats: React.FC<StatsProps> = ({ errors, cpm }) => {
-	return (
-		<div>
-			<span>{`Typos ${errors}  CPM ${cpm}`}</span>
-		</div>
-	);
-};
+  errors: number
+  cpm: number
+  redoLevelHandler: (e: MouseEvent | React.MouseEvent) => void
+  nextLevelHandler: (e: MouseEvent | React.MouseEvent) => void
+}
 
-export default Stats;
+const Stats: React.FC<StatsProps> = ({
+  errors,
+  cpm,
+  redoLevelHandler,
+  nextLevelHandler,
+}) => {
+  return (
+    <div>
+      <span>{`Typos ${errors}  CPM ${cpm}`}</span>
+      <button onClick={redoLevelHandler}>TRY AGAIN</button>
+      <button disabled={isNextLevelDisabled} onClick={nextLevelHandler}>NEXT</button>
+    </div>
+  )
+}
+
+export default Stats

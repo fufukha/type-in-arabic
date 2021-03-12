26fd35cbb9d5acd595115687a2e3f57528771723
fufukha <wafakhalid29@gmail.com>
2021-03-12 18:57:45 -0500

    added badges

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -1,4 +1,15 @@
 import { StatsProps, StatsThreshold, SummaryMessage } from './stats-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faGrinSquint,
+  faGrinStars,
+  faRocket,
+  faTrophy,
+  faBullseye,
+} from '@fortawesome/free-solid-svg-icons'
+import { IconName, library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(faBullseye, faRocket, faTrophy)
 
 const Stats: React.FC<StatsProps> = ({
   errors,
@@ -24,10 +35,28 @@ const Stats: React.FC<StatsProps> = ({
       description: `Must be greater than ${StatsThreshold.cpm} CPM`,
     }
   }
+
+  const hasEarnBadge =
+    (cpm > StatsThreshold.cpm && errors === 0) || cpm > StatsThreshold.cpmGreat
+
+  const badgeName = (): IconName => {
+    if (cpm > StatsThreshold.cpmSuper && errors === 0) {
+      return 'trophy'
+    } else if (cpm > StatsThreshold.cpmGreat) {
+      return 'rocket'
+    } else if (cpm > StatsThreshold.cpm && errors === 0) {
+      return 'bullseye'
+    }
+    //To Typescript error
+    return 'bullseye'
+  }
+
   return (
     <div>
+      <FontAwesomeIcon icon={hasMetThresHold ? faGrinStars : faGrinSquint} />
       <p>{summaryMessages().status}</p>
       {!hasMetThresHold && <p>{summaryMessages().description}</p>}
+      {hasEarnBadge && <FontAwesomeIcon icon={badgeName()} />}
       <span>{`Typos ${errors}  CPM ${cpm}`}</span>
       {hasMetThresHold && (
         <button onClick={nextLevelHandler}>NEXT LEVEL</button>

db425679a2e1759c9d5e79a85a6a6b30de41abb1
fufukha <wafakhalid29@gmail.com>
2021-03-17 16:10:38 -0400

    styled the input text

--- src/components/Keyfield/index.tsx
+++ src/components/Keyfield/index.tsx
@@ -68,7 +68,19 @@ const Keyfield: React.FC = () => {
       alignItems: 'center',
       padding: '0 24px',
       marginBottom: '40px',
+      '& span': {
+        color: 'lightgrey',
+        borderLeft: '1px black solid',
+        animationName: 'cursor',
+        animationDuration: '1.5s',
+        animationIterationCount: 'infinite',
+      }
     },
+    '@keyframes cursor': {
+      '0%,50%': {borderLeftColor: 'rgba(0, 0, 0, 1)'},
+      '51%,100%': {borderLeftColor: 'rgba(0, 0, 0, 0)'},
+    }, 
+    
   }))
 
   const classes = useStyles()

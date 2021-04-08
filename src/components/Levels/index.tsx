5207041a480edd58e37500ece0829430d53da14b
fufukha <wafakhalid29@gmail.com>
2021-04-08 05:42:26 -0400

    format & removed props

--- src/components/Levels/index.tsx
+++ src/components/Levels/index.tsx
@@ -5,18 +5,20 @@ import { levelsTheme } from '../../theme/theme'
 import Keyboard from '../Keyboard'
 import Keyfield from '../Keyfield'
 import Page from '../Page'
-import { useStyles, containerStyles } from './styles'
+import { containerStyles, useStyles } from './styles'
 
-interface Props {}
-
-const Levels: React.FC<Props> = () => {
+const Levels: React.FC = () => {
   const errors = useSelector(errorsSelector)
   const cpm = useSelector(cpmSelector)
 
   const classes = useStyles()
 
   return (
-    <Page themeOptions={levelsTheme} containerMaxWidth={'md'} containerStyles={containerStyles}>
+    <Page
+      themeOptions={levelsTheme}
+      containerMaxWidth={'md'}
+      containerStyles={containerStyles}
+    >
       <Grid className={classes.statBar} container justify='space-between'>
         <Grid item xs={3}>
           <Typography gutterBottom component={'h1'} variant={'h5'}>
